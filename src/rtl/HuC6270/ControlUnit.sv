48651d55bfd1fb1304b231909d90a7a1170a5645
Amolak Nagi <amolakn@ece000.ece.local.cmu.edu>
2018-03-06 21:55:04 -0500

    More changes to VDC based on our discussion about MMIO, still in progress

--- src/rtl/HuC6270/ControlUnit.sv
+++ src/rtl/HuC6270/ControlUnit.sv
@@ -26,6 +26,31 @@ module ControlUnit( input logic   clock,
                                   IRQ_n 
                   );
 
+
+  logic WR_n_prev_in, WR_n_prev;
+  assign WR_n_prev_in = WR_n;
+  Register #($bits(WR_n_prev)) WR_reg(
+                                        .clk(clock),
+                                        .en(1'b1),
+                                        .rst_l(reset_n),
+                                        .clear(),
+                                        .D(WR_n_prev_in),
+                                        .Q(WR_n_prev)
+                                     );
+
+
+  logic RD_n_prev_in, RD_n_prev;
+  assign RD_n_prev_in = RD_n;
+  Register #($bits(RD_n_prev)) RD_reg(
+                                        .clk(clock),
+                                        .en(1'b1),
+                                        .rst_l(reset_n),
+                                        .clear(),
+                                        .D(RD_n_prev_in),
+                                        .Q(RD_n_prev)
+                                     );
+
+
   logic [7:0] status_in, status;
   logic ld_status_reg;
   Register #($bits(status)) status_reg(
@@ -40,23 +65,38 @@ module ControlUnit( input logic   clock,
 
   addr_reg_t addr_in, addr;
   logic ld_addr_reg;
+  assign addr_in = DI;
   Register #($bits(addr), 5'd0) addr_reg(
                                          .clk(clock),
-                                         .en(),
+                                         .en(ld_addr_reg),
                                          .rst_l(),
                                          .clear(),
                                          .D(addr_in),
                                          .Q(addr)
                                        );
 
+  reg_sel_t reg_sel;
+  assign reg_sel = addr_in[4:0];
+
+
+  logic read, write;
 
   always_comb begin
 
+    // TODO: Wire this up to internals of VDC when needed
     status_in = 8'd0;
-    addr_in = 5'd0;
 
     ld_status_reg = 1'd0;
     ld_addr_reg = 1'd0;
+
+    read = 1'd0;
+    write = 1'd0;
+
+    DO = 8'd0;
+
+
+    read  = (~RD_n) && (RD_n != RD_n_prev);
+    write = (~WR_n) && (WR_n != WR_n_prev);
 
     if (~CS_n) begin
 
@@ -66,16 +106,14 @@ module ControlUnit( input logic   clock,
         A_STATUS_ADDR_REG: begin
 
           // If read, fetch data from our Status register
-          if (~RD_n) begin
-            
-            
-            
-
+          if (read) begin
+            DO = status;
           end
 
-          // If write, write data to 
-          else if (~WR_n) begin
-
+          // If write, write data to the address register
+          // (specifying which register we want to write to)
+          else if (write) begin
+            ld_addr_reg = 1'b1;
           end
 
         end
@@ -90,7 +128,10 @@ module ControlUnit( input logic   clock,
         A_DATA_MSB: begin
 
         end
-      
+     
+        default:
+          // RETURN $00 IN UNUSED 2'b01 case!
+        end 
 
       endcase
 
